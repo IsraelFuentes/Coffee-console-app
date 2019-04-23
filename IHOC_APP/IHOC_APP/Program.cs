5df01dbcea3f76aaec4d2882f4cdf6bc217de7f0
Israel <46566412+IsraelFuentes@users.noreply.github.com>
2019-04-23 08:16:07 -0400

    Add files via upload

---
+++ IHOC_APP/IHOC_APP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeLibrary;
+
+namespace IHOC_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Menu();
+           // Console.ReadLine();
+        }
+    }
+}
